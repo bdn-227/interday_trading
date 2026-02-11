2489915d71724eb2dbf12b2ec1c783c6b3a6497c
Niklas Kiel <niklas.b.kiel@gmail.com>
2026-02-11 16:11:48 +0100

    populated the trade engine; started to setup a script for the ema crossover strategy

--- inertia_trading/live_trading.py
+++ inertia_trading/live_trading.py
@@ -1,38 +1,133 @@
 from ib_insync import *
 import pandas as pd
+import random
 
 class LiveIBKREngine:
-    def __init__(self, strategy, contract=Stock(symbol="VUSA", exchange="AEB", currency="EUR")):
+    def __init__(self, strategy, contract):
         self.strategy = strategy
-        self.contract = contract
+        self.contract, self.whatToShow = self.get_contract(**contract)
+
+
+    def get_contract(self, contract_type, symbol, exchange, currency):
+        if contract_type == "Forex":
+            contract = Forex(pair=symbol)
+            whatToShow = "MIDPOINT"
+        elif contract_type == "Index":
+            contract = Index(symbol, exchange, currency)
+            whatToShow = "TRADES"
+        elif contract_type == "Stock":
+            contract = Stock(symbol, exchange, currency)
+            whatToShow = "TRADES"
+        return contract, whatToShow
     
+    
+    def start_IB(self):
+        util.startLoop()
+        self.ib = IB()
+        my_client_id = random.randint(1, 9999)
+        try:
+            self.ib.connect('127.0.0.1', 4002, clientId=my_client_id)
+        except Exception as e:
+            print(f"Error: {e}")
 
-    def get_ib_isntance():
+
+
+    def terminate_IB(self):
+        self.ib.disconnect()
+
+
+
+    def get_markget_data(self):
         pass
 
 
-    def get_markget_data():
-        pass
+
+    def get_cash(self):
+        account_summary = self.ib.accountSummary()
+        cash_balance = 0.0
+        for item in account_summary:
+            if item.tag == 'TotalCashValue' and item.currency == 'EUR':
+                cash_balance = float(item.value)
+                break
+        return cash_balance
 
 
-    def run():
-        
-        # 1. connect to IB gateway
 
-        # 2. clean-up: cancel all open orders from yesterday (especially buy orders)
+    def get_positions(self):
+        positions = self.ib.positions()
+        if positions:
+            df_positions = pd.DataFrame([
+                {
+                    "symbol": p.contract.symbol,
+                    "conId": p.contract.conId,
+                    "exchange": p.contract.exchange,
+                    "size": p.position,
+                    "avg_cost": p.avgCost,
+                    "market_price": 0.0,
+                    "market_value": 0.0,
+                } 
+                for p in positions])
+        return df_positions
 
-        # 3. get account state (portfolio, positions, net worth)
 
-        # 4. fetch data and calculate indicators
 
-        # 5. calculate signals using the strategy
+    def get_portfolio(self):
+        portfolio_items = self.ib.portfolio()
+        if portfolio_items:
+            df_portfolio = pd.DataFrame([
+                {
+                    "symbol": p.contract.symbol,
+                    "size": p.position,
+                    "market_price": p.marketPrice,
+                    "market_value": p.marketValue,
+                    "average_cost": p.averageCost,
+                    "unrealized_pnl": p.unrealizedPNL,
+                    "realized_pnl": p.realizedPNL
+                }
+                for p in portfolio_items])
+        return portfolio_items
 
-        # 6. execute the signals: exits first, new buys etc.
 
-        # 7. log results (trade log if trades happened/were scheduled; log trades what were submitted; target state; portfolio, cash balance, positions, equity curve)
-            # status.json --> override daily, current positions, cash, worth, active 
-            # equity.csv --> Date, NetLiquidity
-            # trades.csv --> Date, Symbol, Action, Price, PnL
-        
-        pass
+
+    def get_open_orders(self):
+        open_orders = self.ib.openOrders()
+        for order in open_orders:
+            print(f"Open Order: {order.action} {order.totalQuantity} @ {order.lmtPrice}")
+        return open_orders
+
+
+
+    def cancel_order(self, trade):
+        self.ib.cancelOrder(trade.order)
+
+
+
+    def market_order(self, action, totalquantity, market_closed):
+        exit_order = MarketOrder(action, totalquantity)
+        if market_closed:
+            exit_order.tif = 'OPG'
+        trade = self.ib.placeOrder(self.contract, exit_order)
+        return trade
+
+
+
+    def limit_order(self, action, totalquantity, lmtprice):
+        entry_order = LimitOrder(action=action, totalQuantity=totalquantity, lmtPrice=lmtprice)
+        entry_order.tif = "DAY"
+        trade = self.ib.placeOrder(self.contract, entry_order)
+        return trade
+
+
+    def limit_order_stop(self, totalquantity, lmtprice, slprice):
+        parent = LimitOrder("BUY", totalquantity, lmtprice)
+        parent.tif = "DAY"
+        parent.orderId = self.ib.client.getReqId()
+        parent.transmit = False
+        stop_loss = StopOrder('SELL', totalquantity, slprice)
+        stop_loss.parentId = parent.orderId
+        stop_loss.transmit = True
+        stop_loss.tif = "GTC"
+        buy = self.ib.placeOrder(self.contract, parent)
+        sell = self.ib.placeOrder(self.contract, stop_loss)
+        return buy, sell
 
--- live.py
+++ live.py
@@ -1,126 +1,35 @@
 # initial script to connect to ib and get a feel for the api
 
 # imports
-from ib_insync import util, IB, Order, LimitOrder, MarketOrder, StopOrder, Stock
-import random
-import pandas as pd
-
-# start IB
-util.startLoop()
-ib = IB()
-
-# # connect to client
-my_client_id = random.randint(1, 9999)
-try:
-    ib.connect('127.0.0.1', 4002, clientId=my_client_id)
-except Exception as e:
-    print(f"Error: {e}")
+from inertia_trading import LiveIBKREngine, EmaCrossoverStrategy
 
 
-# get the account balance
-account_summary = ib.accountSummary()
-cash_balance = 0.0
-for item in account_summary:
-    # TotalCashValue is the raw cash --> we use this
-    # BuyingPower is the leverage. i'll stay away from this. no need to go into debt for now..
-    if item.tag == 'TotalCashValue' and item.currency == 'EUR':
-        cash_balance = float(item.value)
-        break
+# create instance of live trading class
+contract = {"contract_type": "Stock", "symbol": "VUSA", "exchange": "AEB", "currency": "EUR"}
+strategy = EmaCrossoverStrategy(ema_short=5, ema_long=10, length_atr=14, atr_multiplier=3)
+trader = LiveIBKREngine(strategy=strategy, contract=strategy)
 
 
-# get the positions
-positions = ib.positions()
-if positions:
-    df_positions = pd.DataFrame([
-        {
-            "symbol": p.contract.symbol,
-            "conId": p.contract.conId,
-            "exchange": p.contract.exchange,
-            "size": p.position,
-            "avg_cost": p.avgCost,
-            "market_price": 0.0,
-            "market_value": 0.0,
-        } 
-        for p in positions])
+# 1. connect to IB gateway
+trader.start_IB()
 
 
-# get the portfolio worth
-portfolio_items = ib.portfolio()
-if portfolio_items:
-    df_portfolio = pd.DataFrame([
-        {
-            "symbol": p.contract.symbol,
-            "size": p.position,
-            "market_price": p.marketPrice,
-            "market_value": p.marketValue,
-            "average_cost": p.averageCost,
-            "unrealized_pnl": p.unrealizedPNL,
-            "realized_pnl": p.realizedPNL
-        }
-        for p in portfolio_items])
+# 2. clean-up: cancel all open orders from yesterday (especially buy orders)
+
+# 3. get account state (portfolio, positions, net worth)
+
+# 4. fetch data and calculate indicators
+
+# 5. calculate signals using the strategy
+
+# 6. execute the signals: exits first, new buys etc.
+
+# 7. log results (trade log if trades happened/were scheduled; log trades what were submitted; target state; portfolio, cash balance, positions, equity curve)
+    # status.json --> override daily, current positions, cash, worth, active 
+    # equity.csv --> Date, NetLiquidity
+    # trades.csv --> Date, Symbol, Action, Price, PnL
 
 
-# get a list of open orders
-open_orders = ib.openOrders()
-for order in open_orders:
-    print(f"Open Order: {order.action} {order.totalQuantity} @ {order.lmtPrice}")
-
-
-
-# how to cancel orders --> of course, here need to be some conditionswhich to order
-for trade in ib.trades():
-    if trade.orderStatus.status in ('Submitted', 'PreSubmitted'):
-        ib.cancelOrder(trade.order)
-
-
-
-# issue market orders for quick exits
-symbol = "VUSA"
-exchange = "AEB"
-currency = "EUR"
-action = "BUY"
-totalquantity=50
-market_closed = True
-
-contract = Stock(symbol=symbol, exchange=exchange, currency=currency)
-exit_order = MarketOrder(action, totalquantity)
-if market_closed:
-    exit_order.tif = 'OPG'
-ib.placeOrder(contract, exit_order)
-
-
-
-# limit order for strategic entries and exits
-symbol = "VUSA"
-exchange = "AEB"
-currency = "EUR"
-action = "BUY"
-totalquantity=50
-lmtprice = 50
-slprice = 30
-
-contract = Stock(symbol=symbol, exchange=exchange, currency=currency)
-entry_order = LimitOrder(action=action, totalQuantity=totalquantity, lmtPrice=lmtprice)
-entry_order.tif = "DAY"
-trade = ib.placeOrder(contract, entry_order)
-print(trade.orderStatus.status)
-
-
-
-# limit order with internal stop loss
-parent = LimitOrder("BUY", totalquantity, lmtprice)
-parent.tif = "DAY"
-parent.orderId = ib.client.getReqId()
-parent.transmit = False
-stop_loss = StopOrder('SELL', totalquantity, slprice)
-stop_loss.parentId = parent.orderId
-stop_loss.transmit = True
-stop_loss.tif = "GTC"
-ib.placeOrder(contract, parent)
-ib.placeOrder(contract, stop_loss)
-
-
-
-# disconnect
-ib.disconnect()
+# 8. stop the interactive broker instance
+trader.terminate_IB()
 
